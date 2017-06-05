16b06d18087cc8365d10e68cd8b346bc2898d138
abelmeos <amerchan@sura.com.co>
2017-06-05 16:38:00 -0500

    - ejemplo de tipe class.

---
+++ src/test/scala/co/ejemplos/typeclass/Main.scala
@@ -0,0 +1,30 @@
+package co.ejemplos.typeclass
+
+import Printable._
+import PrintableInstances._
+
+/**
+ * Utilizacion del type class.
+ *
+ * Created by abelmeos on 2017/06/05.
+ */
+object Main extends App{
+
+  final case class Cat(name:String, age:Int, color:String)
+
+  implicit val catPrintable = new Printable[Cat] {
+
+    def format(cat:Cat) = {
+      val name = Printable.format(cat.name)
+      val age = Printable.format(cat.age)
+      val color = Printable.format(cat.color)
+      s"$name is a $age year-old $color cat."
+    }
+
+  }
+
+  val cat = Cat("leo", 10, "negro" )
+
+  Printable.print(cat)
+
+}
