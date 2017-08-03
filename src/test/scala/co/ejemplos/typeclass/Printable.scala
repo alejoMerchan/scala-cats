53ee234ecaaada984257f531ab5544987b1248c7
alejoMerchan <alejandromerchan08@mail.com>
2017-08-03 05:15:23 -0500

    - se agrega documentacion.

--- src/test/scala/co/ejemplos/typeclass/Printable.scala
+++ src/test/scala/co/ejemplos/typeclass/Printable.scala
@@ -2,15 +2,32 @@ package co.ejemplos.typeclass
 
 /**
  *
- * Ejemplo de type class  como introduccion a Cats
+ * Ejemplo de type class  como introduccion a Cats.
+  *
+  * Patron de programacion que permite agregar nuevas funcionalidades a librerias, sin utilizar su inherencia o
+  * alterar su codigo fuente.
  *
  * Created by abelmeos on 2017/06/05.
  */
+
+/**
+  * Definicion del typeclass.
+  *
+  * Representa alguna funcionalidad a ser implementada.
+  *
+  * @tparam type A and return String
+  */
 trait Printable[A] {
   def format(value:A):String
 }
 
 
+/**
+  * Instancias del printable para String y para Int.
+  *
+  * Provee la implementacion para cualquier type
+  *
+  */
 object PrintableInstances{
 
   implicit val formatString = new Printable[String] {
@@ -27,6 +44,12 @@ object PrintableInstances{
 
 }
 
+/**
+  * Definicion del objeto con dos interfaces genericas.
+  *
+  * Expone las funcionalidades al usuario.
+  *
+  */
 object Printable {
 
   def format[A](input:A)(implicit p: Printable[A]):String = p.format(input)
